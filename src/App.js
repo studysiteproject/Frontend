38fe02904688427e1c750855782688ca114fe081
Dongyeon1201 <dongyeon1201@gmail.com>
2021-11-16 00:43:24 +0900

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Header from './components/header.js';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import '../css/footer.css'
+
+import Member from './member';
+
+function Footer() {
+
+    return (
+        <div className="Footer">
+            <Member></Member>
+        </div>
+    );
+  
+}
+
+export default Footer;
