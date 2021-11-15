e32084729e7c09a8ebd974ae0cf526f790822dc6
Dongyeon1201 <dongyeon1201@gmail.com>
2021-11-15 23:59:25 +0900

    create header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,64 @@
 import '../css/header.css'
 
+const getCookieValue = (key) => {
+    let cookieKey = key + "="; 
+    let result = "";
+    const cookieArr = document.cookie.split(";");
+    
+    for(let i = 0; i < cookieArr.length; i++) {
+      if(cookieArr[i][0] === " ") {
+        cookieArr[i] = cookieArr[i].substring(1);
+      }
+      
+      if(cookieArr[i].indexOf(cookieKey) === 0) {
+        result = cookieArr[i].slice(cookieKey.length, cookieArr[i].length);
+        return result;
+      }
+    }
+    return result;
+  }
+
 function Header() {
-  return (
-    <div className="Header">
-        <div className="logo-div">
-            <a href="#">
-                <img className="logo-img" src="img/docker.svg"></img>
-            </a>
-        </div>
-        <nav>
-            <ul className="gnb">
-                <li className="text"><a href="">회원가입</a></li>
-                <li className="text"><a href="">로그인</a></li>
-            </ul>
-        </nav>
-    </div>
-  );
+
+    let access_token_exits = getCookieValue("access") ? true : false
+    let user_index = getCookieValue("access") ? true : false
+
+    if (access_token_exits && user_index){
+        return (
+            <div className="Header">
+                <div className="logo-div">
+                    <a href="#">
+                        <img className="logo-img" src="img/docker.svg"></img>
+                    </a>
+                </div>
+                <nav>
+                    <ul className="gnb">
+                        <li className="text"><a href="">스터디관리</a></li>
+                        <li className="icon"><a href=""><img src="img/user_circle.svg"></img></a></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+    else{
+        return (
+            <div className="Header">
+                <div className="logo-div">
+                    <a href="#">
+                        <img className="logo-img" src="img/docker.svg"></img>
+                    </a>
+                </div>
+                <nav>
+                    <ul className="gnb">
+                        <li className="text"><a href="">회원가입</a></li>
+                        <li className="text"><a href="">로그인</a></li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+
+  
 }
 
 export default Header;
