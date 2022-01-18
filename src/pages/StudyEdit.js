725e1bdea562a384bd5b59a61f336d28f86dc3ec
Dongyeon1201 <dongyeon1201@gmail.com>
2022-01-18 23:02:57 +0900

    edit StudyEdit page

--- src/pages/StudyEdit.js
+++ src/pages/StudyEdit.js
@@ -168,6 +168,7 @@ function StudyEditPage(){
         axios.get(`${process.env.REACT_APP_SPRING_API_URL}/study/check/${study_id}`, { headers: header, withCredentials: true, credentials: "include" })
         .then(res => {
             if (res.data['iswriter'] == true){
+
                 // 스터디의 정보를 저장하는 State들의 설정 함수 모음
                 const SetBasicInfo = {
                     setTitle,
@@ -184,20 +185,21 @@ function StudyEditPage(){
                 // 전체 기술 목록 얻어오기
                 TechInfo.GetAllTechList(SetAllTechList);
 
-                return res;
+                
             }
             else {
                 dispatch(ActivePopup("error", "해당 스터디의 작성자가 아닙니다!"));
                 dispatch(UnActivePopup(2));
                 setTimeout(()=>{navigate('/')},2000);
-                return(<></>);     
             }
+
+            return res;
         })
         .catch(error => {
             dispatch(ActivePopup("error", "스터디 작성자 여부를 확인할 수 없습니다!"));
             dispatch(UnActivePopup(2));
             setTimeout(()=>{navigate('/')},2000);
-            return(<></>);
+            return error
         })
 
     },[]);
@@ -219,151 +221,151 @@ function StudyEditPage(){
         }
     },[title, description, category, place, maxman, StudyTechArray])
 
-    if(!islogin && typeof islogin !== 'undefined'){
-        navigate('/login');
-        return(<></>);      
-    }
-    else{
-        return(
-            <>
-                <Header/>
-                    <div className="Frame" style={{paddingTop:'5%'}}>
-                        
-                        {/* 스터디의 제목 설정 */}
-                        <div className='Study-Info'>
-                            <div className='Study-Info-Title item-input'>
-                                <input
-                                    className='input-box'
-                                    placeholder='스터디의 제목을 입력해 주세요.'
-                                    onChange={(e)=>{setTitle(e.target.value)}}
-                                    value={title}
-                                />
-                            </div>
+    // if(!islogin && typeof islogin !== 'undefined'){
+    //     navigate('/login');
+    //     return(<></>);      
+    // }
+    // else{
+    return(
+        <>
+            <Header/>
+                <div className="Frame" style={{paddingTop:'5%'}}>
+                    
+                    {/* 스터디의 제목 설정 */}
+                    <div className='Study-Info'>
+                        <div className='Study-Info-Title item-input'>
+                            <input
+                                className='input-box'
+                                placeholder='스터디의 제목을 입력해 주세요.'
+                                onChange={(e)=>{setTitle(e.target.value)}}
+                                value={title}
+                            />
                         </div>
-    
-                        {/* 스터디의 상세 내용 설정 */}
-                        <div className='Study-Info'>
-    
-                            {/* Select를 사용하는 스터디 설정 */}
-                            <div className='Study-Info-select item'>
-    
-                                {/* 스터디의 모집 분야 선택 */}
-                                <div className='title-item-column'>
-                                    <div className='title'>모집 분야</div>
-                                    <div style={{width: '100%', maxWidth:'300px'}}>
-                                        <SelectBox
-                                            choice={category}
-                                            setChoice={setcategory}
-                                            placeholder={"모집 분야를 선택하세요."}
-                                            options={options.category}
-                                            isSearchable={false}
-                                        />
-                                    </div>
-                                </div>
-    
-                                {/* 스터디의 장소 선택 */}
-                                <div className='title-item-column'>
-                                    <div className='title'>스터디 장소</div>
-                                    <div style={{width: '100%', maxWidth:'300px'}}>
-                                        <SelectBox
-                                            choice={place}
-                                            setChoice={setplace}
-                                            placeholder={"스터디 장소를 선택하세요."}
-                                            options={options.place}
-                                            isSearchable={false}
-                                        />
-                                    </div>
-                                </div>
-    
-                            </div>
-    
-                            {/* 스터디의 기술 스택을 설정하는 부분 */}
-                            <div className='Study-Info-tech item'>
-                                <div className='title-item-column'>
-                                    <div className='title'>기술 스택</div>
-                                    <div className='tech'>
-    
-                                        {/* 현재 스터디의 기술 목록을 나타내는 리스트 */}
-                                        {/* 현재는 item의 X 클릭 시 삭제되도록 설정 */}
-                                        <div className='tech-list'>
-                                            {
-                                                StudyTechArray.map((item)=>{
-                                                    return(
-                                                        <div className="tech-item-box">
-                                                            <div>
-                                                                <img class="sm" src={`${BasicInfo.TECH_ICON_BASE_URL}/${item.category}/${item.icon_url}`} style={{marginRight: '15px'}}></img>
-                                                                {item.name}
-                                                                <img class="sm" src="/img/icon/coolicon.svg" style={{marginLeft: '15px'}}
-                                                                    onClick={()=>{DeleteStudytech(item.id)}}
-                                                                ></img>
-                                                            </div>
-                                                        </div>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-    
-                                        {/* 기술 목록 검색, 선택 박스 */}
-                                        <SearchSelectBox
-                                            choice={techsearch}
-                                            setChoice={setTechsearch}
-                                            placeholder={"원하는 기술을 검색하세요."}
-                                            options={techoption(AllTechList)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            {/* 스터디의 최대 인원수를 설정하는 부분 */}
-                            <div className='Study-Info-users item'>
-                                <div className='title-item-column'>
-                                    <div className='title'>모집 인원</div>
-                                    <div style={{display:'flex', width: '100%', height:'auto', margin:'auto'}}>
-                                        <div style={{width:'80%', marginRight:'30px'}}>
-                                            <Slider
-                                                progress
-                                                defaultValue={maxman}
-                                                max={50}
-                                                onChange={value => {
-                                                    Setmaxman(value);
-                                                }}
-                                            />
-                                        </div>
-                                        <div className='title'>{`${maxman}명`}</div>
-                                    </div>
-                                </div>
-                            </div>
-    
-                        </div>
-    
-                        {/* 스터디의 내용 설정 */}
-                        <div className='Study-Info'>
-                            <div className='Study-Info-Content item-input'>
-                                <input
-                                    className='input-box'
-                                    placeholder="스터디를 소개할 글을 적으세요. &#13;&#10; (무엇을 목표로 하고 어떤 활동을 할 것 인지.)"
-                                    onChange={(e)=>{setdescription(e.target.value)}}
-                                    style={{minHeight:'200px'}}
-                                    value={description}
-                                />
-                            </div>
-                        </div>
-                                                
-                        {/* 스터디 수정하기 버튼 설정 */}
-                        <div className='Study-Info'>
-                            {
-                                // 스터디 수정하기 버튼 활성화 / 비활성화
-                                ableSubmit
-                                ?   <button className='Button-Md' style={{width:'100%'}} onClick={()=>{Submit(title, maxman, description, place, category)}}>스터디 수정하기</button>
-                                :   <button className='Button-Md disabled' style={{width:'100%'}} disabled>스터디 수정하기</button>
-                            }
-                        </div>
-    
                     </div>
-                <Footer/>
-            </>
-        )
-    }
+
+                    {/* 스터디의 상세 내용 설정 */}
+                    <div className='Study-Info'>
+
+                        {/* Select를 사용하는 스터디 설정 */}
+                        <div className='Study-Info-select item'>
+
+                            {/* 스터디의 모집 분야 선택 */}
+                            <div className='title-item-column'>
+                                <div className='title'>모집 분야</div>
+                                <div style={{width: '100%', maxWidth:'300px'}}>
+                                    <SelectBox
+                                        choice={category}
+                                        setChoice={setcategory}
+                                        placeholder={"모집 분야를 선택하세요."}
+                                        options={options.category}
+                                        isSearchable={false}
+                                    />
+                                </div>
+                            </div>
+
+                            {/* 스터디의 장소 선택 */}
+                            <div className='title-item-column'>
+                                <div className='title'>스터디 장소</div>
+                                <div style={{width: '100%', maxWidth:'300px'}}>
+                                    <SelectBox
+                                        choice={place}
+                                        setChoice={setplace}
+                                        placeholder={"스터디 장소를 선택하세요."}
+                                        options={options.place}
+                                        isSearchable={false}
+                                    />
+                                </div>
+                            </div>
+
+                        </div>
+
+                        {/* 스터디의 기술 스택을 설정하는 부분 */}
+                        <div className='Study-Info-tech item'>
+                            <div className='title-item-column'>
+                                <div className='title'>기술 스택</div>
+                                <div className='tech'>
+
+                                    {/* 현재 스터디의 기술 목록을 나타내는 리스트 */}
+                                    {/* 현재는 item의 X 클릭 시 삭제되도록 설정 */}
+                                    <div className='tech-list'>
+                                        {
+                                            StudyTechArray.map((item)=>{
+                                                return(
+                                                    <div className="tech-item-box">
+                                                        <div>
+                                                            <img class="sm" src={`${BasicInfo.TECH_ICON_BASE_URL}/${item.category}/${item.icon_url}`} style={{marginRight: '15px'}}></img>
+                                                            {item.name}
+                                                            <img class="sm" src="/img/icon/coolicon.svg" style={{marginLeft: '15px'}}
+                                                                onClick={()=>{DeleteStudytech(item.id)}}
+                                                            ></img>
+                                                        </div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+
+                                    {/* 기술 목록 검색, 선택 박스 */}
+                                    <SearchSelectBox
+                                        choice={techsearch}
+                                        setChoice={setTechsearch}
+                                        placeholder={"원하는 기술을 검색하세요."}
+                                        options={techoption(AllTechList)}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        
+                        {/* 스터디의 최대 인원수를 설정하는 부분 */}
+                        <div className='Study-Info-users item'>
+                            <div className='title-item-column'>
+                                <div className='title'>모집 인원</div>
+                                <div style={{display:'flex', width: '100%', height:'auto', margin:'auto'}}>
+                                    <div style={{width:'80%', marginRight:'30px'}}>
+                                        <Slider
+                                            progress
+                                            defaultValue={maxman}
+                                            max={50}
+                                            onChange={value => {
+                                                Setmaxman(value);
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='title'>{`${maxman}명`}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* 스터디의 내용 설정 */}
+                    <div className='Study-Info'>
+                        <div className='Study-Info-Content item-input'>
+                            <input
+                                className='input-box'
+                                placeholder="스터디를 소개할 글을 적으세요. &#13;&#10; (무엇을 목표로 하고 어떤 활동을 할 것 인지.)"
+                                onChange={(e)=>{setdescription(e.target.value)}}
+                                style={{minHeight:'200px'}}
+                                value={description}
+                            />
+                        </div>
+                    </div>
+                                            
+                    {/* 스터디 수정하기 버튼 설정 */}
+                    <div className='Study-Info'>
+                        {
+                            // 스터디 수정하기 버튼 활성화 / 비활성화
+                            ableSubmit
+                            ?   <button className='Button-Md' style={{width:'100%'}} onClick={()=>{Submit(title, maxman, description, place, category)}}>스터디 수정하기</button>
+                            :   <button className='Button-Md disabled' style={{width:'100%'}} disabled>스터디 수정하기</button>
+                        }
+                    </div>
+
+                </div>
+            <Footer/>
+        </>
+    )
+    // }
 
 }
 
